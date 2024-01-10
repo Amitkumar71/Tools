0661f705c89960b83af73893136ff1a713472b0e
Amit <115263300+Amitkumar71@users.noreply.github.com>
2024-01-10 22:18:39 +0530

    Creating Cards Dynamically

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv  
+from flask_migrate import Migrate
 import os
 
 load_dotenv()
@@ -10,6 +11,7 @@ app = Flask(__name__)
 # Configure the MySQL database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Define a model for your data
 class SiteSuggestion(db.Model):
@@ -18,6 +20,7 @@ class SiteSuggestion(db.Model):
     email = db.Column(db.String(120), nullable=False)
     site = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=False)
+    image_path = db.Column(db.String(255))  # Adjust the length based on your needs
 
 @app.route('/')
 def home():
@@ -31,9 +34,10 @@ def suggest():
         email = request.form['email']
         site = request.form['site']
         description = request.form['description']
+        image_path = request.form['image_path']
 
         # Create a new SiteSuggestion object and add it to the database
-        suggestion = SiteSuggestion(name=name, email=email, site=site, description=description)
+        suggestion = SiteSuggestion(name=name, email=email, site=site, description=description, image_path=image_path)
         db.session.add(suggestion)
         db.session.commit()
 
@@ -43,7 +47,8 @@ def suggest():
 
 @app.route('/images')
 def images():
-    return render_template('Images.html')
+    data=SiteSuggestion.query.all()
+    return render_template('Images.html',data=data)
 
 @app.route('/thanks')
 def thanks():
@@ -53,3 +58,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create sql tables for our data models
     app.run(debug=True)
+
